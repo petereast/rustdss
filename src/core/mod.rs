8c407e2905889bd31d139ff50298781afe56bacf
Peter East <petereast+gpg@pm.me>
2020-03-10 22:50:06 +0000

    Refactor for better testing

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -15,6 +15,26 @@ struct CoreState {
     keyval: HashMap<String, RespData>,
 }
 
+fn core_logic(state: &mut CoreState, cmd: Command) -> RespData {
+    let response = match cmd {
+        Command::Set(key, value) => {
+            state.keyval.insert(key, value);
+            RespData::ok()
+        }
+        Command::Get(key) => state
+            .keyval
+            .get(&key)
+            .unwrap_or(&RespData::Error("nil".into()))
+            .clone(),
+        Command::FlushAll => {
+            state.keyval.clear();
+            RespData::ok()
+        }
+        _ => RespData::Error("Unknown core cmd".into()),
+    };
+    response
+}
+
 impl Core {
     pub fn start() -> Self {
         // Could do something interesting using a threadpool - key-hash sharding for example
@@ -29,23 +49,7 @@ impl Core {
                 if let Ok(msg) = reciever.recv() {
                     let (cmd, responder) = msg;
 
-                    let response = match cmd {
-                        Command::Set(key, value) => {
-                            state.keyval.insert(key, value);
-                            RespData::ok()
-                        }
-                        Command::Get(key) => state
-                            .keyval
-                            .get(&key)
-                            .unwrap_or(&RespData::Error("nil".into()))
-                            .clone(),
-                        Command::FlushAll => {
-                            state.keyval.clear();
-                            RespData::ok()
-                        }
-                        _ => RespData::Error("Unknown core cmd".into()),
-                    };
-
+                    let response = core_logic(&mut state, cmd);
                     responder
                         .send(response)
                         .expect("[core] can't reply to messages");
@@ -59,5 +63,60 @@ impl Core {
 
     pub fn get_sender(&self) -> SyncSender<Message> {
         self.sender.clone()
+    }
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn set_adds_a_new_key() {
+        let mut state = CoreState {
+            keyval: HashMap::new(),
+        };
+
+        let response = core_logic(
+            &mut state,
+            Command::Set("a".into(), RespData::SimpleStr("hello".into())),
+        );
+
+        assert_eq!(response, RespData::ok());
+        assert_eq!(state.keyval.len(), 1);
+        assert_eq!(
+            state.keyval.get("a"),
+            Some(&RespData::SimpleStr("hello".into()))
+        );
+    }
+
+    #[test]
+    fn flushall_deletes_everything() {
+        let mut state = CoreState {
+            keyval: HashMap::new(),
+        };
+
+        core_logic(
+            &mut state,
+            Command::Set("a".into(), RespData::SimpleStr("hello".into())),
+        );
+        core_logic(
+            &mut state,
+            Command::Set("b".into(), RespData::SimpleStr("goodbye".into())),
+        );
+
+        assert_eq!(state.keyval.len(), 2);
+        assert_eq!(
+            state.keyval.get("a"),
+            Some(&RespData::SimpleStr("hello".into()))
+        );
+        assert_eq!(
+            state.keyval.get("b"),
+            Some(&RespData::SimpleStr("goodbye".into()))
+        );
+
+        core_logic(&mut state, Command::FlushAll);
+
+        assert_eq!(state.keyval.len(), 0);
+        assert_eq!(state.keyval.get("a"), None);
+        assert_eq!(state.keyval.get("b"), None);
     }
 }
