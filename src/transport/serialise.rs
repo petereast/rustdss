591aa9782f29efa82d437b0ddbac5572ff42a109
Peter East <petereast+gpg@pm.me>
2020-03-11 10:58:04 +0000

    Add serialise tests

--- src/transport/serialise.rs
+++ src/transport/serialise.rs
@@ -11,4 +11,39 @@ impl RespData {
             _ => String::from("-Not serialisable"),
         }
     }
+
+    pub fn _bytes(&self) -> std::str::Bytes {
+        // This returns a stream of serialised text
+        unimplemented!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_serialises_simple_strings_properly() {
+        let input = RespData::SimpleStr("hello".into());
+
+        assert_eq!(input.as_string(), "+hello\r\n",);
+    }
+    #[test]
+    fn it_serialises_numbers_properly() {
+        let input = RespData::Number(10);
+
+        assert_eq!(input.as_string(), ":10\r\n");
+    }
+    #[test]
+    fn it_serialises_bulk_strings_properly() {
+        let input = RespData::BulkStr("hello".into());
+
+        assert_eq!(input.as_string(), "$5\r\nhello\r\n");
+    }
+    #[test]
+    fn it_serialises_error_strings_properly() {
+        let input = RespData::Error("error".into());
+
+        assert_eq!(input.as_string(), "-error\r\n");
+    }
 }
