0f44213c039a5f2c842c58209f24d52be91a1236
Peter East <petereast+github@pm.me>
2020-03-24 11:24:04 +0000

    Remove annoying "Hello World!"

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ mod request;
 mod transport;
 
 fn main() -> Result<(), std::io::Error> {
-    println!("Hello, world!");
-
     let core = core::Core::start();
     connection::Connection::start(core.get_sender())?;
 
