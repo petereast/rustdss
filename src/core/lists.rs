2ec9a738c2a33829e241518dd955cafb36f54b75
Peter East <petereast+github@pm.me>
2020-03-26 23:17:04 +0000

    Add list tests

--- src/core/lists.rs
+++ src/core/lists.rs
@@ -53,3 +53,340 @@ pub fn rpop(state: &mut CoreState, key: &Key) -> RespData {
         _ => RespData::nil(),
     }
 }
+#[cfg(test)]
+mod lpush_should {
+    use super::*;
+    use crate::core::CoreState;
+    use std::collections::HashMap;
+
+    #[test]
+    fn create_a_new_list() {
+        let key: String = "key".into();
+        let mut state = CoreState {
+            keyval: HashMap::new(),
+        };
+
+        let response = lpush(&mut state, &key, RespData::SimpleStr("value".into()));
+
+        assert_eq!(
+            state.keyval.get(&key),
+            Some(&RespData::List(
+                vec![RespData::SimpleStr("value".into())].into()
+            ))
+        );
+        assert_eq!(response, RespData::Number(1));
+    }
+
+    #[test]
+    fn push_items_to_the_beginning_of_the_list() {
+        let key: String = "key:00".into();
+
+        let mut keyval = HashMap::new();
+        keyval.insert(
+            key.clone(),
+            RespData::List(
+                vec![
+                    RespData::SimpleStr("first".into()),
+                    RespData::SimpleStr("second".into()),
+                ]
+                .into(),
+            ),
+        );
+        let mut state = CoreState { keyval };
+
+        let response = lpush(
+            &mut state,
+            &key,
+            RespData::SimpleStr("should_be_first".into()),
+        );
+
+        assert_eq!(
+            state.keyval.get(&key),
+            Some(&RespData::List(
+                vec![
+                    RespData::SimpleStr("should_be_first".into()),
+                    RespData::SimpleStr("first".into()),
+                    RespData::SimpleStr("second".into()),
+                ]
+                .into()
+            ))
+        );
+        assert_eq!(response, RespData::Number(3));
+    }
+
+    #[test]
+    fn error_when_the_list_is_a_different_type() {
+        let key: String = "key:00".into();
+
+        let mut keyval = HashMap::new();
+        keyval.insert(key.clone(), RespData::SimpleStr("not_a_list".into()));
+        let mut state = CoreState { keyval };
+
+        let response = lpush(
+            &mut state,
+            &key,
+            RespData::SimpleStr("some_new_data".into()),
+        );
+
+        // Assert that the original item was not mutated
+        assert_eq!(
+            state.keyval.get(&key),
+            Some(&RespData::SimpleStr("not_a_list".into()))
+        );
+        assert_eq!(response, RespData::wrong_type());
+    }
+}
+
+#[cfg(test)]
+mod rpush_should {
+    use super::*;
+    use crate::core::CoreState;
+    use std::collections::HashMap;
+
+    #[test]
+    fn create_a_new_list() {
+        let key: String = "key".into();
+        let mut state = CoreState {
+            keyval: HashMap::new(),
+        };
+
+        let response = rpush(&mut state, &key, RespData::SimpleStr("value".into()));
+
+        assert_eq!(
+            state.keyval.get(&key),
+            Some(&RespData::List(
+                vec![RespData::SimpleStr("value".into())].into()
+            ))
+        );
+        assert_eq!(response, RespData::Number(1));
+    }
+
+    #[test]
+    fn push_items_to_the_end_of_the_list() {
+        let key: String = "key:00".into();
+
+        let mut keyval = HashMap::new();
+        keyval.insert(
+            key.clone(),
+            RespData::List(
+                vec![
+                    RespData::SimpleStr("first".into()),
+                    RespData::SimpleStr("second".into()),
+                ]
+                .into(),
+            ),
+        );
+        let mut state = CoreState { keyval };
+
+        let response = rpush(
+            &mut state,
+            &key,
+            RespData::SimpleStr("should_be_last".into()),
+        );
+
+        assert_eq!(
+            state.keyval.get(&key),
+            Some(&RespData::List(
+                vec![
+                    RespData::SimpleStr("first".into()),
+                    RespData::SimpleStr("second".into()),
+                    RespData::SimpleStr("should_be_last".into()),
+                ]
+                .into()
+            ))
+        );
+        assert_eq!(response, RespData::Number(3));
+    }
+
+    #[test]
+    fn error_when_the_list_is_a_different_type() {
+        let key: String = "key:00".into();
+
+        let mut keyval = HashMap::new();
+        keyval.insert(key.clone(), RespData::SimpleStr("not_a_list".into()));
+        let mut state = CoreState { keyval };
+
+        let response = rpush(
+            &mut state,
+            &key,
+            RespData::SimpleStr("some_new_data".into()),
+        );
+
+        // Assert that the original item was not mutated
+        assert_eq!(
+            state.keyval.get(&key),
+            Some(&RespData::SimpleStr("not_a_list".into()))
+        );
+        assert_eq!(response, RespData::wrong_type());
+    }
+}
+
+#[cfg(test)]
+mod rpop_should {
+    use super::*;
+    use crate::core::CoreState;
+    use std::collections::HashMap;
+
+    #[test]
+    fn return_nil_when_the_list_doesnt_exist() {
+        let key: String = "key".into();
+        let mut state = CoreState {
+            keyval: HashMap::new(),
+        };
+
+        let response = rpop(&mut state, &key);
+
+        assert_eq!(state.keyval.get(&key), None);
+        assert_eq!(response, RespData::nil());
+    }
+
+    #[test]
+    fn pop_items_from_the_end_of_the_list() {
+        let key: String = "key:00".into();
+
+        let mut keyval = HashMap::new();
+        keyval.insert(
+            key.clone(),
+            RespData::List(
+                vec![
+                    RespData::SimpleStr("first".into()),
+                    RespData::SimpleStr("second".into()),
+                    RespData::SimpleStr("should_be_last".into()),
+                ]
+                .into(),
+            ),
+        );
+        let mut state = CoreState { keyval };
+
+        let response = rpop(&mut state, &key);
+
+        assert_eq!(
+            state.keyval.get(&key),
+            Some(&RespData::List(
+                vec![
+                    RespData::SimpleStr("first".into()),
+                    RespData::SimpleStr("second".into()),
+                ]
+                .into()
+            ))
+        );
+        assert_eq!(response, RespData::SimpleStr("should_be_last".into()));
+    }
+
+    #[test]
+    fn returns_nil_when_the_list_is_empty() {
+        let key: String = "key:00".into();
+
+        let mut keyval = HashMap::new();
+        keyval.insert(key.clone(), RespData::List(vec![].into()));
+        let mut state = CoreState { keyval };
+
+        let response = rpop(&mut state, &key);
+
+        assert_eq!(state.keyval.get(&key), Some(&RespData::List(vec![].into())));
+        assert_eq!(response, RespData::nil());
+    }
+
+    #[test]
+    fn error_when_the_list_is_a_different_type() {
+        let key: String = "key:00".into();
+
+        let mut keyval = HashMap::new();
+        keyval.insert(key.clone(), RespData::SimpleStr("not_a_list".into()));
+        let mut state = CoreState { keyval };
+
+        let response = rpop(&mut state, &key);
+
+        // Assert that the original item was not mutated
+        assert_eq!(
+            state.keyval.get(&key),
+            Some(&RespData::SimpleStr("not_a_list".into()))
+        );
+        assert_eq!(response, RespData::wrong_type());
+    }
+}
+
+#[cfg(test)]
+mod lpop_should {
+    use super::*;
+    use crate::core::CoreState;
+    use std::collections::HashMap;
+
+    #[test]
+    fn return_nil_when_the_list_doesnt_exist() {
+        let key: String = "key".into();
+        let mut state = CoreState {
+            keyval: HashMap::new(),
+        };
+
+        let response = lpop(&mut state, &key);
+
+        assert_eq!(state.keyval.get(&key), None);
+        assert_eq!(response, RespData::nil());
+    }
+
+    #[test]
+    fn pop_items_from_the_end_of_the_list() {
+        let key: String = "key:00".into();
+
+        let mut keyval = HashMap::new();
+        keyval.insert(
+            key.clone(),
+            RespData::List(
+                vec![
+                    RespData::SimpleStr("first".into()),
+                    RespData::SimpleStr("second".into()),
+                    RespData::SimpleStr("should_be_last".into()),
+                ]
+                .into(),
+            ),
+        );
+        let mut state = CoreState { keyval };
+
+        let response = lpop(&mut state, &key);
+
+        assert_eq!(
+            state.keyval.get(&key),
+            Some(&RespData::List(
+                vec![
+                    RespData::SimpleStr("second".into()),
+                    RespData::SimpleStr("should_be_last".into()),
+                ]
+                .into()
+            ))
+        );
+        assert_eq!(response, RespData::SimpleStr("first".into()));
+    }
+
+    #[test]
+    fn returns_nil_when_the_list_is_empty() {
+        let key: String = "key:00".into();
+
+        let mut keyval = HashMap::new();
+        keyval.insert(key.clone(), RespData::List(vec![].into()));
+        let mut state = CoreState { keyval };
+
+        let response = lpop(&mut state, &key);
+
+        assert_eq!(state.keyval.get(&key), Some(&RespData::List(vec![].into())));
+        assert_eq!(response, RespData::nil());
+    }
+
+    #[test]
+    fn error_when_the_list_is_a_different_type() {
+        let key: String = "key:00".into();
+
+        let mut keyval = HashMap::new();
+        keyval.insert(key.clone(), RespData::SimpleStr("not_a_list".into()));
+        let mut state = CoreState { keyval };
+
+        let response = lpop(&mut state, &key);
+
+        // Assert that the original item was not mutated
+        assert_eq!(
+            state.keyval.get(&key),
+            Some(&RespData::SimpleStr("not_a_list".into()))
+        );
+        assert_eq!(response, RespData::wrong_type());
+    }
+}
